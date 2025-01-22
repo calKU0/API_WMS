0529b96cf66915c14a81810c5fe13a8070fb283d
Krzysztof Kurowski <228528@student.uek.krakow.pl>
2025-01-22 08:31:52 +0100

    change enum names

--- Data/Enums/DocumentType.cs
+++ Data/Enums/DocumentType.cs
@@ -2,51 +2,48 @@
 
 namespace APIWMS.Data.Enums
 {
-    /// <summary>
-    /// Typy dokumentów w systemie ERP
-    /// </summary>
     public enum DocumentType
     {
-        [EnumMember(Value = "1601 - Dokument magazynowy (WM)")]
+        [EnumMember(Value = "1601 - WM")]
         WM = 1601,
 
-        [EnumMember(Value = "1089 - Przyjęcie magazynowe (PM)")]
+        [EnumMember(Value = "1089 - PM")]
         PM = 1089,
 
-        [EnumMember(Value = "1602 - Przesunięcie magazynowe (MP)")]
+        [EnumMember(Value = "1602 - MP")]
         MP = 1602,
 
-        [EnumMember(Value = "1093 - Awizo dostawy (AWD)")]
+        [EnumMember(Value = "1093 - AWD")]
         AWD = 1093,
 
-        [EnumMember(Value = "1605 - Zwrot magazynowy (ZWM)")]
+        [EnumMember(Value = "1605 - ZWM")]
         ZWM = 1605,
 
-        [EnumMember(Value = "2041 - Faktura sprzedaży korygująca (FSK)")]
+        [EnumMember(Value = "2041 - FSK")]
         FSK = 2041,
 
-        [EnumMember(Value = "2033 - Faktura sprzedaży (FS)")]
+        [EnumMember(Value = "2033 - FS")]
         FS = 2033,
 
-        [EnumMember(Value = "2001 - Wydanie zewnętrzne (WZ)")]
+        [EnumMember(Value = "2001 - WZ")]
         WZ = 2001,
 
-        [EnumMember(Value = "1616 - Rozchód wewnętrzny (RW)")]
+        [EnumMember(Value = "1616 - RW")]
         RW = 1616,
 
-        [EnumMember(Value = "1617 - Przyjęcie wewnętrzne (PW)")]
+        [EnumMember(Value = "1617 - PW")]
         PW = 1617,
 
-        [EnumMember(Value = "1603 - Między magazynowe wydanie (MMW)")]
+        [EnumMember(Value = "1603 - MMW")]
         MMW = 1603,
 
-        [EnumMember(Value = "2034 - Paragon (PA)")]
+        [EnumMember(Value = "2034 - PA")]
         PA = 2034,
 
-        [EnumMember(Value = "2037 - Faktura sprzedaży eksportowej (FSE)")]
+        [EnumMember(Value = "2037 - FSE")]
         FSE = 2037,
 
-        [EnumMember(Value = "1604 - Między magazynowe przyjęcie (MMP)")]
+        [EnumMember(Value = "1604 - MMP")]
         MMP = 1604
     }
 
