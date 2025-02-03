5bbd872d319837a8798e878421eeb8c570056ba9
Krzysztof Kurowski <228528@student.uek.krakow.pl>
2025-02-03 10:30:54 +0100

    add new enum

---
+++ Data/Enums/DocumentStatus.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.Serialization;
+
+namespace APIWMS.Data.Enums
+{
+    public enum DocumentStatus
+    {
+        [EnumMember(Value = "-2 - Cancel")]
+        Cancel = -2,
+
+        [EnumMember(Value = "-1 - Delete")]
+        Delete = -1,
+
+        [EnumMember(Value = "0 - Confirm")]
+        Confirm = 0,
+
+        [EnumMember(Value = "1 - Bufor")]
+        Bufor = 1
+    }
+}
